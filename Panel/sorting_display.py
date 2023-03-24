005c07066421b73a2e0d6cb6a1f93fa4169ffe15
emircan-bostanci <emircan_bostanci@hotmail.com>
2023-03-24 04:37:04 +0300

    sorting display

--- Panel/sorting_display.py
+++ Panel/sorting_display.py
@@ -2,12 +2,14 @@ from Panel.console_panel import console_panel
 
 class sorting_display(console_panel):
     currently_algorithm = None
-    cursor = console_panel.c
     def __init__(self,currently_algorithm):
         self.currently_algorithm = currently_algorithm
-
+    def render(self):
+        console_panel.render()
     def visualise(self):
-        #Algoritma Her adimi iterate etmeli 
+        steps = 0
+        for i in range(steps):
+            print(steps)
 
 
 
